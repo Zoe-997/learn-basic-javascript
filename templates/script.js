1026bbde2ab882e71b56ad8ca03604f89d1bca29
ZoeTrinh <zoetrinh1997@gmail.com>
2020-03-30 22:02:36 +0700

    add file templates

---
+++ templates/script.js
@@ -0,0 +1,38 @@
+console.log('test');
+
+// biến
+var arr1 = 'ZoeTrinh';
+var arr2 = 10;
+var arr3 = 20;
+var arr4 = arr2 + arr3;
+console.log(arr1);
+console.log(arr2);
+console.log(arr3);
+console.log(arr4);
+
+// toán tử
+var x = 1, y = 2;
+console.log(x + y);
+
+var z ='hello ';
+console.log(z);
+console.log(z + y);
+
+var a = 'ZoeTrinh';
+console.log(z + a);
+console.log(x - y);
+console.log(x*y*y);
+console.log(y/x);
+
+var t = 5;
+console.log(t%y);
+
+
+function run(){
+	t =  t - 1;
+	console.log('value of t:' + t);
+
+	// ap dụng cho slide
+	y = y + 1;
+	console.log('value of y:' + (y%2));
+}
