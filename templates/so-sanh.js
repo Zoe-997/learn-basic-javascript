749557811e461840087a272b3f54c37ada070995
ZoeTrinh <zoetrinh1997@gmail.com>
2020-04-06 11:44:03 +0700

    add file so-sanh.js

---
+++ templates/so-sanh.js
@@ -0,0 +1,18 @@
+// so sánh
+var diem = 5;
+if(diem < 5){
+	console.log('duoi trung binh');
+}
+else{
+	console.log('diem tren trung binh');
+}
+
+
+// toán tử so sánh
+//  	== bằng
+//  	> lơn hơn
+// 	 	< nhỏ hơn
+//  	=< nhỏ hơn hoặc bằng
+//  	>= lớn hơn hoặc bằng
+//  	&& và
+//  	|| hoặc
