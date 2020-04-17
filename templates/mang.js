f52a51d37e255ab2ab4cfcd2a1dd7e5039841c90
ZoeTrinh <zoetrinh1997@gmail.com>
2020-04-17 21:09:21 +0700

    javascript to unit 10 : array

---
+++ templates/mang.js
@@ -0,0 +1,17 @@
+var hv = ["minh", "ha"];
+console.log(hv);
+
+// các cách khai báo mảng
+
+/*C1:*/   var hv = ["minh", "ha"];
+
+/*C2:*/   var hv2 = [];
+hv2[0] = "minh";
+hv2[2] = "ha";
+
+
+/*C3: khuyên ít dùng*/   var hv3 = new Array('minh', 'ha');
+console.log('hv3[0]');
+console.log('hv3[1]');
+
+
